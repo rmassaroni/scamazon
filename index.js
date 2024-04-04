3a6294efa846666985b55ca8c6228008d26ef6cc
rjm <robjmass@gmail.com>
2024-04-04 14:03:47 -0400

    unnamed commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ async function main() {
     const browser = await playwright.chromium.launch({
         headless: false
     });
+    //console.log(browser.options.executablePath);
     const page = await browser.newPage();
     await page.goto('https://www.amazon.com/gp/goldbox/?_encoding=UTF8&_encoding=UTF8&ref_=dlx_gate_sd_dcl_vai_dt_pd_gw_unk&pd_rd_w=CFRbL&content-id=amzn1.sym.26a365d6-3002-449e-bfff-1848c98a3efd&pf_rd_p=26a365d6-3002-449e-bfff-1848c98a3efd&pf_rd_r=H9YY7NJ9T2Y1WE233XB3&pd_rd_wg=wvJ1b&pd_rd_r=404d67c0-4ae3-4bc3-ae98-1ab59543f3a7');
     const deals = await page.$$eval(".DealContent-module__truncate_sWbxETx42ZPStTc9jwySW", listElms => {
